ea7c46e3b25e2a6cce24ad44a62481ea405720d7
unknown <abu040810@gmail.com>
2023-11-29 13:39:41 +0500

    oj

---
+++ public/locales/en/index.jsx
@@ -0,0 +1,38 @@
+
+export const en = {
+
+    home: "Home",
+    about: "About",
+    travel: "Historical places and museums",
+    feedbacks: "Feedbacks",
+
+    introTitle: "The best travel facilities in Andijan",
+    introText: "We offer you an unforgettable trip",
+
+    popularTitle: "Popular destinations",
+    popularCard: "Hazrati Imam OTA Complex",
+    popularText: "In our Andijan there are many recreation areas, tourist centers, natural centers with healing air, much attention is paid to lakes and rivers. You will be surprised by the wide range of accommodation in Uzbekistan - hotels, private boarding houses, boutique hotels, hostels, student campuses, here you can choose the most affordable price.",
+    popularStatistics: "3323+ (for example)",
+    popularTravels: "Happy travelers",
+    popularRoutes: "Routes created",
+
+    funTitle: "Entertainment programs",
+    funText: "andijan offers not only excursions to historical architectural monuments, but also places where you can have fun with family and friends.",
+    funAsk: "What awaits you?",
+    funInfo: "Travel agencies in Andjan region will provide you with:!!",
+    funSpeed: "Speed",
+    funSafe: "Safety",
+    funComfort: "Comfort and Convenience",
+
+    offerTitle: "Andijan offers all types of tourism",
+
+    footerContact: "Contacts",
+    footerAuth: "Copyright © 2023 • All rights reserved.",
+    footerSocials: "Social media",
+    footerConData: "Contact details",
+    address : "Address",
+    phone: "Phone number",
+    email: "Email"
+
+
+}
---
+++ public/locales/uz/index.jsx
@@ -0,0 +1,37 @@
+
+export const uz = {
+
+    home: "Bosh sahifa",
+    about: "Andijon haqida",
+    travel: "Tarixiy obida va muzeylar",
+    feedbacks: "Agroturizm",
+    introTitle: "Andijondagi eng yaxshi sayohat obyektlari",
+    introText: "Biz sizga unutilmas sayohatni taklif qilamiz",
+
+    
+    popularTitle: "Mashhur yo'nalishlar",
+    popularCard: "Hazrati Imom OTA majmuasi",
+    popularText: "viloyatimizda ko'plab dam olish maskanlari, sayyohlik markazlari, shifobaxsh havosi bo'lgan tabiiy markazlar mavjud, ko'l va daryolarga katta e'tibor beriladi. Andijonnda turar joyning keng assortimenti – mehmonxonalar, xususiy pansionatlar, butik mehmonxonalar, hostellar, talabalar shaharchalari sizni hayratda qoldiradi, bu yerda siz eng hamyonbop narxni tanlashingiz mumkin.",
+    popularStatistics: "3323+ (masalan)",
+    popularTravels: "Baxtli sayohatchilar",
+    popularRoutes: "Marshrutlar yaratilgan",
+
+    
+    funTitle: "O'yin-kulgi",
+    funText: "Andijon nafaqat tarixiy me’moriy yodgorliklarga ekskursiyalarni, balki oilangiz va do‘stlaringiz davrasida hordiq chiqarishingiz mumkin bo‘lgan joylarni ham taklif etadi.",
+    funAsk: "Sizni nima kutmoqda?",
+    funInfo: "Andjon viloyatidagi travel agentliklar sizni shular bilan taminlaydi  :!",
+    funSpeed: "Tezlik",
+    funSafe: "Xavfsizlik",
+    funComfort: "Komfort va qulaylik",
+
+    offerTitle: "Andijonda turizmning barcha turlarini taklif etamiz",
+
+    footerContact: "Kontaktlar",
+    footerAuth: "Mualliflik huquqi © 2023 • Barcha huquqlar himoyalangan.",
+    footerSocials: "Ijtimoiy tarmoqlar",
+    footerConData: "Aloqa ma'lumotlari",
+    address : "Manzil",
+    phone: "Telefon raqami",
+    email: "Elektron pochta"
+}
